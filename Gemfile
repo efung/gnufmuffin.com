09f2b375cacb7b7bc606d9967bb4dbed8d7d60ca
Eric Fung <efung@acm.org>
2014-06-24 08:25:52 -0400

    Initial checkin based on Hyde
    - Added background image to sidebar
    - Tweaked CSS
    - Added generators for month and year archives

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'github-pages'
+gem 'jekyll'
+gem 'narray', :git => "https://github.com/tonyarnold/narray"
+gem 'gsl', :git => "https://github.com/tonyarnold/rb-gsl"
+
---
+++ _plugins/year_archive_generator.rb
@@ -0,0 +1,39 @@
+module Jekyll
+
+  class YearArchiveGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'year_archive_index'
+        site.posts.group_by{ |c| c.date.year }.each do |year, posts|
+          posts = posts.sort_by { |p| -p.date.to_f }
+          archive_dir = year.to_s
+          paginate(site, archive_dir, posts, year)
+        end
+      end
+    end
+
+    def paginate(site, dir, posts, year)
+      pager = Pager.new(site, 1, posts, 1)
+      archive_path = "/" + dir
+      path = archive_path
+
+      newpage = YearArchiveIndex.new(site, site.source, path, archive_path, year, posts)
+      newpage.pager = pager
+      site.pages << newpage
+    end
+  end
+
+  class YearArchiveIndex < Page
+    def initialize(site, base, dir, base_path, year, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'year_archive_index.html')
+      self.data['title'] = "#{year} Archive"
+      self.data['base'] = base_path
+    end
+  end
+end
